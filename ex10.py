128233f96af2a91d56b9b7f0e06c20fced4b5163
cmundt88 <chris.mundt@gmail.com>
2015-07-20 11:51:11 -0600

    adding exercises 6-11

---
+++ ex10.py
@@ -0,0 +1,18 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split \non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies.  'Cool!'
+\t* Catnip\n\t* Grass
+'''
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+print "here is a test of the spe\acial bell thingy"
+print "here's a vert\vical tab"
+print "here's a test %r" % 'of the \n ability of the troubleshooting character'
